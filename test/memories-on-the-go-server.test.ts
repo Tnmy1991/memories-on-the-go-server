e2ac55812d7b62a3082ce5a3f00f7e37ff526ea8
Tonmoy Malik <tonmoy.malik@gmail.com>
2024-07-28 18:11:19 +0530

    Initial commit

---
+++ test/memories-on-the-go-server.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MemoriesOnTheGoServer from '../lib/memories-on-the-go-server-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/memories-on-the-go-server-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MemoriesOnTheGoServer.MemoriesOnTheGoServerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
