c452abd9ad999e6ba51fb8389981073eac5d55c4
Tonmoy Malik <tonmoy.malik@gmail.com>
2024-08-15 00:01:02 +0530

    client deployment added

--- lib/memories-on-the-go-server-stack.ts
+++ lib/memories-on-the-go-server-stack.ts
@@ -3,13 +3,37 @@ import * as s3 from "aws-cdk-lib/aws-s3";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
 import * as lambdaEventSources from "aws-cdk-lib/aws-lambda-event-sources";
 
 export class MemoriesOnTheGoServerStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Create S3 bucket
+    // Create an S3 bucket to host the client
+    const clientBucket = new s3.Bucket(this, "ClientBucket", {
+      websiteIndexDocument: "index.html",
+      websiteErrorDocument: "error.html",
+      blockPublicAccess: {
+        blockPublicAcls: false,
+        ignorePublicAcls: false,
+        restrictPublicBuckets: false,
+        blockPublicPolicy: false,
+      },
+      publicReadAccess: true,
+    });
+
+    // Deploy website content to S3 bucket
+    new s3deploy.BucketDeployment(this, "DeployClient", {
+      destinationBucket: clientBucket,
+      sources: [
+        s3deploy.Source.asset(
+          "./ui-client/dist/memories-on-the-go-client/browser"
+        ),
+      ],
+    });
+
+    // Create an S3 bucket for image storage
     const imageBucket = new s3.Bucket(this, "ImageBucket", {
       bucketName: "memories-on-the-go-image-bucket",
       cors: [
@@ -133,5 +157,10 @@ export class MemoriesOnTheGoServerStack extends cdk.Stack {
     userTable.grantReadWriteData(users);
     imageTable.grantReadWriteData(images);
     imageTable.grantReadWriteData(thumbnail);
+
+    // Output the website URL
+    new cdk.CfnOutput(this, "ClientURL", {
+      value: `http://${clientBucket.bucketName}.s3-website-${cdk.Aws.REGION}.amazonaws.com`,
+    });
   }
 }
